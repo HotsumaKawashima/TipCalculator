f13d51a72ac24acd2e0462aa197a4abc0e2b262b
hotsuma kawashima <hkawashimajob1992@gmail.com>
2020-05-09 22:50:18 -0700

    comp

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var billAmountTextField: UITextField!
+    @IBOutlet var tipAmountLabel: UILabel!
+    @IBOutlet var tipPercentageTextField: UITextField!
+    @IBOutlet var adjustTipPercentage: UISlider!
+    
+     override func viewDidLoad() {
+       super.viewDidLoad()
+    
+       NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+       NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+     }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        view.frame.origin.y -= 200
     }
-
-
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func onCalc(_ sender: UIButton) {
+        update()
+    }
+    
+    @IBAction func onSlide(_ sender: UISlider) {
+        tipPercentageTextField.text = String(Int(sender.value))
+        update()
+    }
+    
+    func update() {
+        if let amount = Float(billAmountTextField.text!), let percent = Float(tipPercentageTextField.text!) {
+            tipAmountLabel.text = String.init(format: "$ %.2f", amount * percent / 100)
+        }
+    }
+    
 }
 
